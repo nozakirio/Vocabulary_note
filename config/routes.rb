2eafd282d4520e6d87f000349876659fe5e86bf4
EC2 Default User <wada.rio1001@gmail.com>
2020-10-23 09:06:47 +0000

    [Add]ヘッダー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'homes#top'
-  get 'home/about' => 'homes#about'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  root :to => "homes#top"
+  get 'home/about' => 'homes#about'
+  resources :books, only: [:create, :index, :show, :edit]
 end
